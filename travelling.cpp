8fb764744c1785471d307fa5ea7ab1145ff8ef4b
VALiUM <93570937+VALiUMgithub@users.noreply.github.com>
2023-03-30 10:21:51 +0530

    Update travelling.cpp

--- travelling.cpp
+++ travelling.cpp
@@ -1,3 +1,33 @@
+// Begin
+//    Define a variable vr = 4 universally.
+//    Declare an integer function TSP to implement Travelling salesman Problem.
+//    Declare a graph grph[][] as a 2D matrix and variable p to the integer datatype.
+//    Pass them as a parameter.
+//    Declare variable ver to the vector datatype.
+//    for (int i = 0; i < vr; i++)
+//       if (i != p) then
+//          Call push_back(i) function to store the value of all vertex except source vertex.
+//          Initialize m_p = INT_MAX to store minimum weight of a graph.
+//       do
+//          Declare cur_pth, k to the integer datatype.
+//             initialize cur_pth = 0.
+//             initialize k = p.
+//          for (int i = 0; i < ver.size(); i++)
+//             cur_pth += grph[k][ver[i]].
+//             k = ver[i].
+//          cur_pth += grph[k][p].
+//          m_p = min(m_p, cur_pth) to update the value of minimum weight.
+//          while (next_permutation(ver.begin(), ver.end())).
+//          Return m_p.
+//    Declare a graph grph[][] as a 2D matrix to the integer datatype.
+//       Initialize values of grph[][] graph.
+//    Declare variable p to the integer datatype.
+//       Initialize p = 0.
+//    Print “The result is: ”.
+//    Print the return value of TSP() function.
+// End.
+
+
 #include<stdio.h>
 int ary[10][10],completed[10],n,cost=0;
 void takeInput()
