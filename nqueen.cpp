be4dbbd95e686a3d9acf8499aa19270e384ac194
VALiUM <93570937+VALiUMgithub@users.noreply.github.com>
2023-03-30 10:23:29 +0530

    Update nqueen.cpp

--- nqueen.cpp
+++ nqueen.cpp
@@ -1,3 +1,27 @@
+// Begin
+//    if there is a queen at the left of current col, then
+//       return false
+//    if there is a queen at the left upper diagonal, then
+//       return false
+//    if there is a queen at the left lower diagonal, then
+//       return false;
+//    return true //otherwise it is valid place
+// End
+// solveNQueen(board, col)
+// Begin
+//    if all columns are filled, then
+//       return true
+//    for each row of the board, do
+//       if isValid(board, i, col), then
+//          set queen at place (i, col) in the board
+//          if solveNQueen(board, col+1) = true, then
+//             return true
+//          otherwise remove queen from place (i, col) from board.
+//       done
+//    return false
+// End
+
+
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
